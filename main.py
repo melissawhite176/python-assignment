75d23fbdeb9a41a71c34ecc582399c2791a4424f
Melissa White <melissawhite842@gmail.com>
2021-03-16 21:02:09 +0000

    finished exercises (not including oop)

--- main.py
+++ main.py
@@ -137,8 +137,6 @@ password = getpass.getpass()
 
 print('Password entered:', password) 
 print(len(password))
-min = 10
-max = 15
 length = len(password)
 if (length < 10) or (length > 15):
   print("Password must be between 10-15 characters. Try again.")
@@ -205,4 +203,453 @@ def factorial(x):
 num = 3
 print("Factorial of", num, "is", factorial(num))
 
-print("Scope- Can variables defined in a function be used in another function?")
+# Scope- Can variables defined in a function be used in another function? -no
+
+def varInFunc():
+  balance = 100
+  print(balance - 50)
+
+#cannot print this due to scope
+# print(balance) 
+# print(varInFunc().balance)
+
+
+
+# Lists
+states = ["Alaska","Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio","Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+
+print("all states")
+for state in states:
+  print(state)
+
+print("states that start with M")
+for state in states:
+  if state.startswith("M"):
+    print(state)
+
+
+
+
+# List Operations
+y = [6, 4, 2]
+print('y:', y)
+y.append(12)
+y.append(8)
+y.append(4)
+print('y:', y)
+y[1] = 3
+print('y:', y)
+
+
+
+
+
+#Sorting list
+x = [(3,6), (4,7), (5,9), (8,4), (3,1)]
+x.sort()
+print('simple sort:', x)
+
+import operator
+x = [(3,6), (4,7), (5,9), (8,4), (3,1)]
+x.sort(key = operator.itemgetter(0))
+print('sort on first element:', x)
+x.sort(key = operator.itemgetter(1))
+print('now sorted on second element:', x)
+
+
+
+# Range Function
+thousand = list(range(1, 1001, 1))
+print('max:', max(thousand))
+print('min:', min(thousand))
+
+evens = list(range(2, 11, 2))
+print('even list:', evens)
+odds = list(range(1, 10 , 2))
+print('odd list:', odds)
+
+
+
+
+
+
+# Dictionary
+countriesDict = {
+	'AD': 'Andorra',
+	'AE': 'United Arab Emirates',
+	'AF': 'Afghanistan',
+	'AG': 'Antigua & Barbuda',
+	'AI': 'Anguilla',
+	'AL': 'Albania',
+	'AM': 'Armenia',
+	'AN': 'Netherlands Antilles',
+	'AO': 'Angola',
+	'AQ': 'Antarctica',
+	'AR': 'Argentina',
+	'AS': 'American Samoa',
+	'AT': 'Austria',
+	'AU': 'Australia',
+	'AW': 'Aruba',
+	'AZ': 'Azerbaijan',
+	'BA': 'Bosnia and Herzegovina',
+	'BB': 'Barbados',
+	'BD': 'Bangladesh',
+	'BE': 'Belgium',
+	'BF': 'Burkina Faso',
+	'BG': 'Bulgaria',
+	'BH': 'Bahrain',
+	'BI': 'Burundi',
+	'BJ': 'Benin',
+	'BM': 'Bermuda',
+	'BN': 'Brunei Darussalam',
+	'BO': 'Bolivia',
+	'BR': 'Brazil',
+	'BS': 'Bahama',
+	'BT': 'Bhutan',
+	'BU': 'Burma (no longer exists)',
+	'BV': 'Bouvet Island',
+	'BW': 'Botswana',
+	'BY': 'Belarus',
+	'BZ': 'Belize',
+	'CA': 'Canada',
+	'CC': 'Cocos (Keeling) Islands',
+	'CF': 'Central African Republic',
+	'CG': 'Congo',
+	'CH': 'Switzerland',
+	'CI': 'Côte D\'ivoire (Ivory Coast)',
+	'CK': 'Cook Iislands',
+	'CL': 'Chile',
+	'CM': 'Cameroon',
+	'CN': 'China',
+	'CO': 'Colombia',
+	'CR': 'Costa Rica',
+	'CS': 'Czechoslovakia (no longer exists)',
+	'CU': 'Cuba',
+	'CV': 'Cape Verde',
+	'CX': 'Christmas Island',
+	'CY': 'Cyprus',
+	'CZ': 'Czech Republic',
+	'DD': 'German Democratic Republic (no longer exists)',
+	'DE': 'Germany',
+	'DJ': 'Djibouti',
+	'DK': 'Denmark',
+	'DM': 'Dominica',
+	'DO': 'Dominican Republic',
+	'DZ': 'Algeria',
+	'EC': 'Ecuador',
+	'EE': 'Estonia',
+	'EG': 'Egypt',
+	'EH': 'Western Sahara',
+	'ER': 'Eritrea',
+	'ES': 'Spain',
+	'ET': 'Ethiopia',
+	'FI': 'Finland',
+	'FJ': 'Fiji',
+	'FK': 'Falkland Islands (Malvinas)',
+	'FM': 'Micronesia',
+	'FO': 'Faroe Islands',
+	'FR': 'France',
+	'FX': 'France, Metropolitan',
+	'GA': 'Gabon',
+	'GB': 'United Kingdom (Great Britain)',
+	'GD': 'Grenada',
+	'GE': 'Georgia',
+	'GF': 'French Guiana',
+	'GH': 'Ghana',
+	'GI': 'Gibraltar',
+	'GL': 'Greenland',
+	'GM': 'Gambia',
+	'GN': 'Guinea',
+	'GP': 'Guadeloupe',
+	'GQ': 'Equatorial Guinea',
+	'GR': 'Greece',
+	'GS': 'South Georgia and the South Sandwich Islands',
+	'GT': 'Guatemala',
+	'GU': 'Guam',
+	'GW': 'Guinea-Bissau',
+	'GY': 'Guyana',
+	'HK': 'Hong Kong',
+	'HM': 'Heard & McDonald Islands',
+	'HN': 'Honduras',
+	'HR': 'Croatia',
+	'HT': 'Haiti',
+	'HU': 'Hungary',
+	'ID': 'Indonesia',
+	'IE': 'Ireland',
+	'IL': 'Israel',
+	'IN': 'India',
+	'IO': 'British Indian Ocean Territory',
+	'IQ': 'Iraq',
+	'IR': 'Islamic Republic of Iran',
+	'IS': 'Iceland',
+	'IT': 'Italy',
+	'JM': 'Jamaica',
+	'JO': 'Jordan',
+	'JP': 'Japan',
+	'KE': 'Kenya',
+	'KG': 'Kyrgyzstan',
+	'KH': 'Cambodia',
+	'KI': 'Kiribati',
+	'KM': 'Comoros',
+	'KN': 'St. Kitts and Nevis',
+	'KP': 'Korea, Democratic People\'s Republic of',
+	'KR': 'Korea, Republic of',
+	'KW': 'Kuwait',
+	'KY': 'Cayman Islands',
+	'KZ': 'Kazakhstan',
+	'LA': 'Lao People\'s Democratic Republic',
+	'LB': 'Lebanon',
+	'LC': 'Saint Lucia',
+	'LI': 'Liechtenstein',
+	'LK': 'Sri Lanka',
+	'LR': 'Liberia',
+	'LS': 'Lesotho',
+	'LT': 'Lithuania',
+	'LU': 'Luxembourg',
+	'LV': 'Latvia',
+	'LY': 'Libyan Arab Jamahiriya',
+	'MA': 'Morocco',
+	'MC': 'Monaco',
+	'MD': 'Moldova, Republic of',
+	'MG': 'Madagascar',
+	'MH': 'Marshall Islands',
+	'ML': 'Mali',
+	'MN': 'Mongolia',
+	'MM': 'Myanmar',
+	'MO': 'Macau',
+	'MP': 'Northern Mariana Islands',
+	'MQ': 'Martinique',
+	'MR': 'Mauritania',
+	'MS': 'Monserrat',
+	'MT': 'Malta',
+	'MU': 'Mauritius',
+	'MV': 'Maldives',
+	'MW': 'Malawi',
+	'MX': 'Mexico',
+	'MY': 'Malaysia',
+	'MZ': 'Mozambique',
+	'NA': 'Namibia',
+	'NC': 'New Caledonia',
+	'NE': 'Niger',
+	'NF': 'Norfolk Island',
+	'NG': 'Nigeria',
+	'NI': 'Nicaragua',
+	'NL': 'Netherlands',
+	'NO': 'Norway',
+	'NP': 'Nepal',
+	'NR': 'Nauru',
+	'NT': 'Neutral Zone (no longer exists)',
+	'NU': 'Niue',
+	'NZ': 'New Zealand',
+	'OM': 'Oman',
+	'PA': 'Panama',
+	'PE': 'Peru',
+	'PF': 'French Polynesia',
+	'PG': 'Papua New Guinea',
+	'PH': 'Philippines',
+	'PK': 'Pakistan',
+	'PL': 'Poland',
+	'PM': 'St. Pierre & Miquelon',
+	'PN': 'Pitcairn',
+	'PR': 'Puerto Rico',
+	'PT': 'Portugal',
+	'PW': 'Palau',
+	'PY': 'Paraguay',
+	'QA': 'Qatar',
+	'RE': 'Réunion',
+	'RO': 'Romania',
+	'RU': 'Russian Federation',
+	'RW': 'Rwanda',
+	'SA': 'Saudi Arabia',
+	'SB': 'Solomon Islands',
+	'SC': 'Seychelles',
+	'SD': 'Sudan',
+	'SE': 'Sweden',
+	'SG': 'Singapore',
+	'SH': 'St. Helena',
+	'SI': 'Slovenia',
+	'SJ': 'Svalbard & Jan Mayen Islands',
+	'SK': 'Slovakia',
+	'SL': 'Sierra Leone',
+	'SM': 'San Marino',
+	'SN': 'Senegal',
+	'SO': 'Somalia',
+	'SR': 'Suriname',
+	'ST': 'Sao Tome & Principe',
+	'SU': 'Union of Soviet Socialist Republics (no longer exists)',
+	'SV': 'El Salvador',
+	'SY': 'Syrian Arab Republic',
+	'SZ': 'Swaziland',
+	'TC': 'Turks & Caicos Islands',
+	'TD': 'Chad',
+	'TF': 'French Southern Territories',
+	'TG': 'Togo',
+	'TH': 'Thailand',
+	'TJ': 'Tajikistan',
+	'TK': 'Tokelau',
+	'TM': 'Turkmenistan',
+	'TN': 'Tunisia',
+	'TO': 'Tonga',
+	'TP': 'East Timor',
+	'TR': 'Turkey',
+	'TT': 'Trinidad & Tobago',
+	'TV': 'Tuvalu',
+	'TW': 'Taiwan, Province of China',
+	'TZ': 'Tanzania, United Republic of',
+	'UA': 'Ukraine',
+	'UG': 'Uganda',
+	'UM': 'United States Minor Outlying Islands',
+	'US': 'United States of America',
+	'UY': 'Uruguay',
+	'UZ': 'Uzbekistan',
+	'VA': 'Vatican City State (Holy See)',
+	'VC': 'St. Vincent & the Grenadines',
+	'VE': 'Venezuela',
+	'VG': 'British Virgin Islands',
+	'VI': 'United States Virgin Islands',
+	'VN': 'Viet Nam',
+	'VU': 'Vanuatu',
+	'WF': 'Wallis & Futuna Islands',
+	'WS': 'Samoa',
+	'YD': 'Democratic Yemen (no longer exists)',
+	'YE': 'Yemen',
+	'YT': 'Mayotte',
+	'YU': 'Yugoslavia',
+	'ZA': 'South Africa',
+	'ZM': 'Zambia',
+	'ZR': 'Zaire',
+	'ZW': 'Zimbabwe',
+	'ZZ': 'Unknown or unspecified country',
+}
+
+print(countriesDict)
+
+# Read file
+filename = "file.txt"
+
+with open(filename) as f:
+    content = f.readlines()
+
+numLine = 1
+for line in content:
+  print(numLine, line)
+  numLine += 1
+
+
+  #if file doesn't exist: -FileNotFoundError prin
+# filename = "nonexistent.txt"
+
+# with open(filename) as f:
+#     content = f.readlines()
+
+# numLine = 1
+# for line in content:
+#   print(numLine, line)
+#   numLine += 1
+
+
+# Write File
+f = open("test.txt", "a")
+f.write("Take it easy")
+f.close()
+
+
+
+
+#Nested loops
+row1 = ["1L", "1M", "1R"]
+row2 = ["2L", "2M", "2R"]
+row3 = ["3L", "3M", "3R"]
+
+for position1 in row1:
+  for position2 in row2:
+    for position3 in row3:
+      print(position1, position2, position3)
+
+persons = ["John", "Marissa", "Pete", "Dayton"]
+
+length = len(persons)
+
+for person1 in persons:
+  for person2 in persons:
+    if(person1 != person2):
+      print(person1 + " meets " + person2)
+
+  #If a normal for loop finishes in n steps O(n), how many steps has a nested loop? - O(N * M)
+
+
+
+
+
+
+
+# Slices
+pizzas = ['Hawaii', 'Pepperoni', 'Fromaggi', 'Napolitana', 'Diavoli']
+mySlice = pizzas[3:4]
+print(mySlice)
+
+hello = "Hello World"
+print(hello[6:11])
+
+
+
+
+# Multiple Return
+def abReturns(a, b):
+  abSum = a + b
+  print(a, b, abSum)
+  return a, b, abSum
+
+abReturns(5, 10)
+
+def fiveReturns(a, b, c, d, e):
+  print(a, b, c, d, e)
+  return(a, b, c, d, e)
+
+fiveReturns(1, 2, 3, 4, 5)
+
+
+
+
+
+# Scope
+balance = 100
+def reduceAmount(withdrawAmount):
+  global balance
+  print('previous balance:', balance)
+  balance -= withdrawAmount
+
+reduceAmount(25)
+print('new balance:', balance)
+
+def funcWithLocal(addAmount):
+  localVar = 10
+  print('previous:', localVar)
+  localVar += addAmount
+  print('updated:', localVar)
+
+funcWithLocal(15)
+# print(localVar)
+
+# Time and date
+import time
+timenow = time.localtime(time.time())
+
+year,month,day = timenow[0:3]
+print(str(year) + '/' + str(month) + '/' + str(day))
+
+  #invalid keyboard input
+try: 
+  addFive = input("add a number to 5 (must be a number):")
+  addFive = int(addFive) + 5
+  print(addFive)
+except:
+  print("Invalid input. Must provide a number")
+
+  #file open error
+try:
+  open("nonexistent.txt")
+except FileNotFoundError:
+  print("file does not exist!")
+  #when to not use try-except: if there is no error/exception to catch and no need to handle them
+  
